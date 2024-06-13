840f2f448d8669bcb01b500cfd9d1087c3c953f1
Sam <samuelkoh17@gmail.com>
2024-06-13 16:24:12 +0800

    feat: implemented stripe webhook to receive payment endpoint, and materialize CSS CDN

--- stripe-backend/index.js
+++ stripe-backend/index.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const uuid = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 
 
@@ -14,6 +14,40 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+
+app.post('/payment', (req, res) => {
+  const { product, token } = req.body;
+  console.log("Product", product);
+  console.log("Price", product.price);
+  //ensure user is not charged twice
+  const idempotencyKey = uuidv4(); 
+  return stripe.customers.create({
+    email: token.email,
+    source: token.id,
+    // description: product.name,
+    // metadata: {
+    //   idempotencyKey
+    // }
+  }).then((customer) => {
+    console.log("Customer created", customer);
+    stripe.charges.create({
+      amount: product.price * 100,
+      currency: 'usd',
+      customer: customer.id,
+      receipient_email: token.email,
+      description: product.name,
+      shipping: {
+        name: token.card.name,
+        address: {
+          country: token.card.address_country,
+      }
+    }
+  }, {idempotencyKey })
+  
+    .then(result => res.status(200).json(result))
+    .catch((err => res.status(500).json(err)));
+  })
+});
 
 
 //listen 
